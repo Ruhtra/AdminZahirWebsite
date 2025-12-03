8c60f8fea485a7d33d8d9bb9f03a4126533fc100
ruhtra <kawanarthurskate@gmail.com>
2025-12-03 02:10:56 -0300

    Cron configuration to vercel with next no ended

--- src/app/api/cron/mypackage.ts
+++ src/app/api/cron/mypackage.ts
@@ -1,4 +1,8 @@
-import puppeteer from 'puppeteer'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import puppeteer from 'puppeteer-core'
+
+
+const isVercel = !!process.env.VERCEL_ENV;
 
 class Instagram {
     _name = "";
@@ -7,42 +11,96 @@ class Instagram {
         this._name = name;
     }
 
+
+
     converterInstagramFollowers(followers: string): number {
-        // if (typeof followers !== 'string') {
-        //     return 0;
-        // }
+        const sanitizedFollowers = followers.toLowerCase().trim();
+        const regex = /([\d,.]+)\s?(mil|mi)?/;
+        const match = sanitizedFollowers.match(regex);
 
-        followers = followers.trim().toLowerCase();
+        if (match) {
+            const getNumber = match[1].replace(",", ".")
+            const unit = match[2];
 
-        if (followers.includes('m')) {
-            const number = parseFloat(followers.replace('m', '').trim());
-            return number * 1000000;
+            let number = parseFloat(getNumber);
+
+            if (unit === "mil") {
+                number *= 1000;
+            }
+            if (unit === "mi") {
+                number *= 1000000;
+            }
+
+            return Math.round(number);
         }
 
-        if (followers.includes('k')) {
-            const number = parseFloat(followers.replace('k', '').trim());
-            return number * 1000;
-        }
-
-        return parseInt(followers.replace(/[^0-9]/g, ''));
+        return 0;
     }
+    // converterInstagramFollowers(followers: string): number {
+    //     // if (typeof followers !== 'string') {
+    //     //     return 0;
+    //     // }
+    //     console.log('aaaaaaaa');
+    //     console.log(followers);
+
+
+    //     if (!followers) return 0;
+
+
+    //     followers = followers.trim().toLowerCase();
+
+    //     if (followers.includes('mi')) {
+    //         const number = parseFloat(followers.replace('mi', '').trim());
+    //         return number * 1000000;
+    //     }
+
+    //     if (followers.includes('mil')) {
+    //         const number = parseFloat(followers.replace('mil', '').trim());
+    //         return number * 1000;
+    //     }
+
+    //     return parseInt(followers.replace(/[^0-9]/g, ''));
+    // }
 
     async getInstagramFollowers() {
-        const browser = await puppeteer.launch({ headless: true });
+        // let browser = null;
+
+        const { puppeteer, launchOptions } = await getOpt()
+
+
+        const browser = await puppeteer.launch(launchOptions);
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
         await page.goto(`https://www.instagram.com/${this._name}/`);
-        await page.waitForSelector('meta[name="description"]');
-        const followers = await page.$eval('meta[name="description"]', (element) => {
-            const content = element.getAttribute('content');
+        await page.waitForSelector('.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.xyejjpt.x15dsfln.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xl565be.xo1l8bm.x1roi4f4.x2b8uid.x10wh9bi.xpm28yp.x8viiok.x1o7cslx');
 
-            return content?.split(' ')[0];
+        const followers = await page.evaluate(() => {
+            const elements = document.querySelectorAll('.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.xyejjpt.x15dsfln.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xl565be.xo1l8bm.x1roi4f4.x2b8uid.x10wh9bi.xpm28yp.x8viiok.x1o7cslx');
+
+            const result: any[] = [];
+            elements.forEach((element) => {
+                const textContent = element?.textContent;
+                const text = textContent ? textContent.trim() : "";
+
+                if (text.includes("seguidores") || text.includes("followers")) {
+                    result.push(text.split(/\s+/).slice(0, 2).join(' '));
+
+                }
+            });
+            return result;
         });
+
+
+        // const followers = await page.$eval('meta[name="description"]', (element: any) => {
+        //     const content = element.getAttribute('content');
+
+        //     return content?.split(' ')[0];
+        // });
         if (!followers) {
             await browser.close();
             throw new Error("Não foi possível encontrar a quantidade de seguidores");
         }
-        const followersCount = this.converterInstagramFollowers(followers);
+        const followersCount = this.converterInstagramFollowers(followers[0]);
         await browser.close();
         return followersCount;
     }
@@ -76,11 +134,15 @@ class Tiktok {
     }
 
     async getTikTokFollowers() {
-        const browser = await puppeteer.launch();
+        const { puppeteer, launchOptions } = await getOpt()
+
+
+
+        const browser = await puppeteer.launch(launchOptions);
         const page = await browser.newPage();
         await page.goto(`https://www.tiktok.com/${this._name}`);
         await page.waitForSelector('strong[data-e2e="followers-count"]');
-        const followers = await page.$eval('strong[data-e2e="followers-count"]', (element) => element.innerText);
+        const followers = await page.$eval('strong[data-e2e="followers-count"]', (element: any) => element.innerText);
         await browser.close();
         return this.converterTikTokFollowers(followers);
     }
@@ -118,7 +180,10 @@ class YouTube {
     }
 
     async getYouTubeFollowers() {
-        const browser = await puppeteer.launch({ headless: true });
+        const { puppeteer, launchOptions } = await getOpt()
+
+
+        const browser = await puppeteer.launch(launchOptions);
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
         await page.goto(`https://www.youtube.com/${this._name}`, { waitUntil: 'networkidle2' });
@@ -128,12 +193,15 @@ class YouTube {
         const inscritos = await page.evaluate(() => {
             const elements = document.querySelectorAll('.yt-core-attributed-string.yt-content-metadata-view-model__metadata-text.yt-core-attributed-string--white-space-pre-wrap.yt-core-attributed-string--link-inherit-color:nth-child(1)');
 
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             const result: any[] = [];
+            console.log('elementos:');
+
             elements.forEach((element) => {
                 const textContent = element?.textContent;
                 const text = textContent ? textContent.trim() : "";
-                if (text.includes("inscritos")) {
+                console.log(text);
+
+                if (text.includes("inscritos") || text.includes("subscribers") || text.includes("subscritores")) {
                     result.push(text);
                 }
             });
@@ -189,3 +257,25 @@ type SocialMediaFollowersOptions = {
     youtube?: string;
     sumTotal?: boolean;
 };
+async function getOpt(): Promise<{ puppeteer: any; launchOptions: any; }> {
+    if (isVercel) {
+        const chromium = (await import("@sparticuz/chromium")).default;
+        return {
+
+            puppeteer: await import("puppeteer-core"),
+            launchOptions: {
+                headless: true,
+                args: chromium.args,
+                executablePath: await chromium.executablePath(),
+            }
+        }
+    } else {
+        return {
+            puppeteer: await import("puppeteer"),
+            launchOptions: {
+                headless: true,
+            }
+        }
+    }
+}
+
